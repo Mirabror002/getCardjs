2a605af5c67cdeb19c548ee0966f3f725598c6e2
Mirabror Rashidov [FE-17] <122270982+Mirabror002@users.noreply.github.com>
2023-01-27 17:34:41 +0500

    Add files via upload

---
+++ lesson-8/script.js
@@ -0,0 +1,28 @@
+//  
+// let str ='Open Web Academy '
+// function repeatStr(){
+//   return str.repeat(15)
+// }
+// console.log(repeatStr())
+
+
+
+let str='8600123456789123'
+
+function getCriditCard(num){
+if (num >= 0 && num.length == 16 && num !== String){
+  console.log(num.slice(0, 4)+"********"+num.slice(12,16));
+  if(num.slice(0, 4) == 8600){
+    console.log('Uzcard kartasi')
+    } else if(num.slice(0, 4) == 9860){
+      console.log('Humo kartasi')
+    }
+}else{
+console.log('Karta raqam xato kiritildi')
+}
+}
+getCriditCard(str)
+
+
+
+
